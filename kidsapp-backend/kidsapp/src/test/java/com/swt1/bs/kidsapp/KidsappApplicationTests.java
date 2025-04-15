869c8a892debcabef805d469b81d1879780dbe61
Devin Korkmaz <devin.korkmaz003@stud.fh-dortmund.de>
2025-04-15 21:41:09 +0200

    Spring boot aufgesetzt

---
+++ kidsapp-backend/kidsapp/src/test/java/com/swt1/bs/kidsapp/KidsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swt1.bs.kidsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
