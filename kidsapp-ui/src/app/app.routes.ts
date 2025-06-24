38cb927c64235c9b2d7269f814696a00b29f640d
dev_Korkmaz <devin.korkmaz@gfi.ihk.de>
2025-06-24 13:38:05 +0200

    admin Seite erstellt

--- kidsapp-ui/src/app/app.routes.ts
+++ kidsapp-ui/src/app/app.routes.ts
@@ -1,10 +1,13 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import { Leaflet } from './leaflet/leaflet';
 import { Welcome } from './welcome/welcome';
 import { Another } from './another/another';
+import {Admin} from './admin/admin';
+
 
 export const routes: Routes = [
   { path: 'leaflet', component: Leaflet },
    { path: 'test', component: Another },
-  { path: '', component: Welcome }, 
+  { path: 'admin', component: Admin },
+  { path: '', component: Welcome },
 ];
