6c439271a7c46b4906c90be5828a382224e56e19
dev_Gulatz <jonas.gulatz@gfi.ihk.de>
2025-06-25 12:55:27 +0200

    new Component and maybe docker-compose port fix

--- kidsapp-ui/src/app/app.routes.ts
+++ kidsapp-ui/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Leaflet } from './leaflet/leaflet';
 import { Welcome } from './welcome/welcome';
 import { Another } from './another/another';
 import {Admin} from './admin/admin';
+import {UserEventVerwaltungComponent} from './user-event-verwaltung/user-event-verwaltung.component';
 
 
 export const routes: Routes = [
   { path: 'leaflet', component: Leaflet },
-   { path: 'test', component: Another },
+  { path: 'test', component: Another },
   { path: 'admin', component: Admin },
+  { path: 'uev', component: UserEventVerwaltungComponent },
   { path: '', component: Welcome },
 ];
---
+++ kidsapp-ui/src/app/user-event-verwaltung/user-event-verwaltung.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-event-verwaltung',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-event-verwaltung.component.html',
+  styleUrl: './user-event-verwaltung.component.css'
+})
+export class UserEventVerwaltungComponent {
+
+}
