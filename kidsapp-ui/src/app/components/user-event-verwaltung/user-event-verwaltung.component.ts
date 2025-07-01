81cb94487b64c95f1f46e30e809b498b2ac4b8c4
dev_Gulatz <jonas.gulatz@gfi.ihk.de>
2025-07-01 14:08:09 +0200

    component with cards and more images

--- kidsapp-ui/src/app/components/user-event-verwaltung/user-event-verwaltung.component.ts
+++ kidsapp-ui/src/app/components/user-event-verwaltung/user-event-verwaltung.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {
   MatCard,
   MatCardActions,
   MatCardContent,
   MatCardHeader,
+  MatCardModule,
   MatCardSubtitle,
   MatCardTitle
 } from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-user-event-verwaltung',
@@ -17,11 +20,26 @@ import {
     MatCardContent,
     MatCardActions,
     MatCardTitle,
-    MatCardSubtitle
+    MatCardSubtitle,
+    MatButtonModule,
+    MatCardModule
   ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './user-event-verwaltung.component.html',
   styleUrl: './user-event-verwaltung.component.css'
 })
 export class UserEventVerwaltungComponent {
 
+  cards: string[] = ["1", "2", "3"];
+
+  constructor(public router: Router) {
+  }
+
+  cancelBooking() {
+    console.log('Booking canceled')
+  }
+
+  navigate(url: string) {
+    this.router.navigateByUrl(url);
+  }
 }
