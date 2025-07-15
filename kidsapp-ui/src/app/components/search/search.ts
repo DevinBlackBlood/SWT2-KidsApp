f80ed7c9e102eed4b22540838263bf75d98745e3
dev_Korkmaz <devin.korkmaz@gfi.ihk.de>
2025-07-15 20:19:56 +0200

    Navbar geändert und Home Seite Veranstaltungen ausgeben

--- kidsapp-ui/src/app/components/search/search.ts
+++ kidsapp-ui/src/app/components/search/search.ts
@@ -1,19 +1,50 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import {
+  MatCard,
+  MatCardActions,
+  MatCardAvatar,
+  MatCardContent,
+  MatCardHeader,
+  MatCardSubtitle, MatCardTitle
+} from '@angular/material/card';
+import {MatButton} from '@angular/material/button';
+import {Veranstaltung, VeranstaltungControllerService} from '../../../../src-gen/bsclient';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, MatCard, MatButton, MatCardActions, MatCardAvatar, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle],
   templateUrl: `./search.html`,
   styleUrls: ['./search.css']
 })
-export class Search {
+export class Search implements  OnInit {
   @Output() searchEvent = new EventEmitter<string>();
   @Output() locationFound = new EventEmitter<any>();
 
   searchTerm = '';
   searchResults: any[] = [];
 
+  constructor(private veranstaltungControllerService:VeranstaltungControllerService) {
+  }
+  datasource: Veranstaltung[] = []
+filteredDatasource: Veranstaltung[] = [];
+  ngOnInit() {
+
+    this.veranstaltungControllerService.veranstaltungen().subscribe( e => {
+
+      this.datasource = e;
+      this.filteredDatasource = e;
+    })
+
+  }
+
+  search() {
+
+    this.filteredDatasource = this.datasource.filter(item => {
+      return item.veranstaltungName!.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+        item.beschreibung!.toLowerCase().includes(this.searchTerm.toLowerCase());
+    });
+  }
 }
